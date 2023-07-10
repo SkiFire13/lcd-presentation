9fa9dc3c1f632427bb870a6fdb95df2a73bcf255
Giacomo Stevanato <giaco.stevanato@gmail.com>
2023-07-10 11:41:56 +0200

    Update mpsc-1 example

--- examples/src/bin/mpsc-1-unbounded.rs
+++ examples/src/bin/mpsc-1-unbounded.rs
@@ -4,21 +4,33 @@ use std::sync::mpsc;
 fn main() {
     let (sender, receiver) = mpsc::channel();
 
-    std::thread::scope(|scp| {
-        scp.spawn(move || {
-            for _ in 0..10 {
-                let value = random::<u32>() % 10;
-                println!("Producer: sending {value}");
-                sender.send(value).unwrap();
-            }
-        });
-        scp.spawn(|| {
-            let value = receiver.recv().unwrap();
-            println!("Consumer: received {value}");
-
-            for value in receiver {
-                println!("Consumer: received {value}");
-            }
-        });
+    let sender_handle = std::thread::spawn(move || {
+        std::thread::yield_now();
+        for _ in 0..10 {
+            let value = random::<u32>() % 10;
+            println!("Producer: sending {value}");
+            sender.send(value).unwrap();
+        }
     });
+
+    let receiver_handle = std::thread::spawn(move || {
+        std::thread::yield_now();
+
+        let value = receiver.recv().unwrap();
+        println!("Consumer: received {value}");
+
+        if let Ok(value) = receiver.try_recv() {
+            println!("Consumer: immediately received {value}");
+        } else {
+            println!("Consumer: no value was ready");
+        }
+
+        for value in receiver {
+            println!("Consumer: received {value}");
+        }
+    });
+
+    // When main ends all the threads are stopped, so join the handles.
+    sender_handle.join().unwrap();
+    receiver_handle.join().unwrap();
 }
