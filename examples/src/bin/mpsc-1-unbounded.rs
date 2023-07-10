f0a10e474847341501f59c73d5e382dc118abfdb
Giacomo Stevanato <giaco.stevanato@gmail.com>
2023-07-10 12:05:42 +0200

    Comment mpsc examples

--- examples/src/bin/mpsc-1-unbounded.rs
+++ examples/src/bin/mpsc-1-unbounded.rs
@@ -2,13 +2,20 @@ use rand::random;
 use std::sync::mpsc;
 
 fn main() {
+    // Unbounded channels are created using `channel`.
+    // The two halves of the channel are returned in a tuple.
     let (sender, receiver) = mpsc::channel();
 
+    // Channels work nicely with `std::thread::spawn` because they don't need
+    // access to the environment.
     let sender_handle = std::thread::spawn(move || {
+        // yield just to give a chance to the other thread to start
         std::thread::yield_now();
+
         for _ in 0..10 {
             let value = random::<u32>() % 10;
             println!("Producer: sending {value}");
+            // `send` sends a value to the channel
             sender.send(value).unwrap();
         }
     });
@@ -16,15 +23,21 @@ fn main() {
     let receiver_handle = std::thread::spawn(move || {
         std::thread::yield_now();
 
+        // `recv` blocks until a value is available in the channel.
+        // returns an error if the channel is empty and no sender exists.
         let value = receiver.recv().unwrap();
         println!("Consumer: received {value}");
 
+        // `try_recv` immediately return a value if it's available
+        // otherwise it returns an error.
         if let Ok(value) = receiver.try_recv() {
             println!("Consumer: immediately received {value}");
         } else {
             println!("Consumer: no value was ready");
         }
 
+        // Receivers can be iterated over, yielding all the elements sent
+        // until no sender exists anymore.
         for value in receiver {
             println!("Consumer: received {value}");
         }
--- examples/src/bin/mpsc-2-bounded.rs
+++ examples/src/bin/mpsc-2-bounded.rs
@@ -2,8 +2,15 @@ use rand::random;
 use std::sync::mpsc;
 
 fn main() {
+    // Bounded channels are created using `sync_channel`.
+    // Note that the given value is just a lower bound.
+    //
+    // The name comes from a historical flaw in the API of `channel`
+    // (that is, `Sender` from `channel` is not `Sync`).
+    // The rest of the API is the same as `channel`.
     let (sender, receiver) = mpsc::sync_channel(2);
 
+    // Scope is still cleaner compared to `std::thread::spawn`.
     std::thread::scope(|scp| {
         scp.spawn(move || {
             for _ in 0..10 {
@@ -12,9 +19,15 @@ fn main() {
                 sender.send(value).unwrap();
             }
         });
-        scp.spawn(|| {
+        scp.spawn(move || {
             let value = receiver.recv().unwrap();
             println!("Consumer: received {value}");
+
+            if let Ok(value) = receiver.try_recv() {
+                println!("Consumer: immediately received {value}");
+            } else {
+                println!("Consumer: no value was ready");
+            }
 
             for value in receiver {
                 println!("Consumer: received {value}");
